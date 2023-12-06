566cbe4c013039ea2ab92be9582424794621abe7
Mikenziokah <mikenziokah@gmail.com>
2023-12-06 11:55:00 -0800

    fabric file

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,14 +7,21 @@ web_static folder of your AirBnB Clone repo, using the function do_pack
 
 from datetime import datetime
 from fabric.operations import local
+import os.path
 
 
 def do_pack():
-    """ compress files"""
-    local("mkdir -p versions")
-    result = ("tar -cvzf versions/web_static_{}.tgz web_static"
-              .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
-              capture=True)
-    if result.failed:
+    """compressing the files """
+    dt = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                         dt.month,
+                                                         dt.day,
+                                                         dt.hour,
+                                                         dt.minute,
+                                                         dt.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
         return None
-    return result
+    return file
