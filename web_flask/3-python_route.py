8c0465a6ccb91befb50c3124b760a25f323cf279
Mikenziokah <mikenziokah@gmail.com>
2023-12-31 13:54:37 +0000

    python is cool

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""starting web application"""
+"""starting a web application"""
 from flask import Flask
-app = Flask(__name__)
+app =  Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
@@ -20,6 +20,13 @@ def hello_hbnb():
 def hello_text(text):
     """ Third Route that display C and text"""
     return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def hello_python(text):
+    """ Fourth Route that display C and text"""
+    return "Python {}".format(text.replace("_", " "))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
